1850d24de2e64bcebfd70c0a632bf727dcb8f698
elif-nisa <elifnisaeryigit000@gmail.com>
2024-06-12 15:25:12 +0300

    Displaying text from existing photos to the terminal

--- ocr_example.py
+++ ocr_example.py
@@ -4,7 +4,7 @@ import pytesseract
 # Tesseract'ın yüklü olduğu yolu belirtin (macOS ve Linux için gerekli değilse bu satırı kaldırabilirsiniz)
 # pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
 
-img_path = 'images/photo7.jpeg'
+img_path = 'images/photo1.jpeg'
 img = Image.open(img_path)
 
 text = pytesseract.image_to_string(img)
